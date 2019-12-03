80526c704bb38bfd3f2ba9fd27f62b68e110577b
Alexey <besedinalexey@gmail.com>
2019-12-03 23:48:18 +0300

    Guides service

--- src/services/guides.ts
+++ src/services/guides.ts
@@ -1,10 +1,25 @@
 import axios from 'axios';
 import {serverURL} from "./server-address";
 import Guide from "../interfaces/guide";
+import PartGuide from "../interfaces/part-guide";
 
 export function getAllGuides(): Promise<Array<Guide>> {
     return new Promise((resolve, reject) => {
         axios.get(`${serverURL}/guide/all`)
+            .then(data => resolve(data.data)).catch(reject);
+    });
+}
+
+export function getGuideImageName(guideId: number): Promise<string> {
+    return new Promise((resolve, reject) => {
+        axios.get(`${serverURL}/guide/img?guideId=${guideId}`)
+            .then(data => resolve(data.data)).catch(reject);
+    });
+}
+
+export function getPartGuides(guideId: number): Promise<Array<PartGuide>> {
+    return new Promise((resolve, reject) => {
+        axios.get(`${serverURL}/guide/parts?guideId=${guideId}`)
             .then(data => resolve(data.data)).catch(reject);
     });
 }
