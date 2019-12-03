ce28c5d387d6d2501a64da60205749c2a5dd18d9
Alexey <besedinalexey@gmail.com>
2019-12-03 23:48:28 +0300

    ViewGuideView

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import './App.sass';
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import GuidesContainer from "./components/home-page/guides-container/guides-container";
+import GuidesContainerView from "./components/home-page-view/guides-container-view/guides-container-view";
+import ViewGuideView from "./components/guide-view/view-guide-view/view-guide-view";
 
 class App extends Component {
     render() {
@@ -11,7 +12,8 @@ class App extends Component {
             <Router>
                 <div>
                     <Switch>
-                        <Route path="/" component={GuidesContainer}/>
+                        <Route exact path="/" component={GuidesContainerView} />
+                        <Route path="/guide/:id" component={ViewGuideView} />
                     </Switch>
                 </div>
             </Router>
