aabb31794995d2c3040f836a991adcd03f9fb9c5
Alexey <besedinalexey@gmail.com>
2019-12-03 23:47:36 +0300

    CSS -> SASS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import './App.sass';
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
