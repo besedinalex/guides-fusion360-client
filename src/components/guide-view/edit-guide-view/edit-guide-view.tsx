bec887dd6fe799df03eeceec2e489eba8a45e5af
Alexey <besedinalexey@gmail.com>
2020-04-06 20:31:53 +0300

    Created basic layout for /edit page

--- src/components/guide-view/edit-guide-view/edit-guide-view.tsx
+++ src/components/guide-view/edit-guide-view/edit-guide-view.tsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Glyphicon from '@strongdm/glyphicon';
 import HeaderComponent from "../../header-component/header-component";
 
 export default class EditGuideView extends Component {
@@ -6,6 +7,28 @@ export default class EditGuideView extends Component {
         return (
             <div>
                 <HeaderComponent />
+
+                <div className="container margin-after-header py-5 justify-content-center">
+                    <div className="list-group w-50 m-auto">
+
+                        <button className="list-group-item list-group-item-success">
+                            <span className="float-left">
+                                Какой-то гайд
+                            </span>
+                            <button className="float-right badge badge-white">
+                                <Glyphicon glyph="chevron-down" />
+                            </button>
+                            <button className="float-right badge badge-white">
+                                <Glyphicon glyph="chevron-up" />
+                            </button>
+                        </button>
+
+
+                        <button className="list-group-item list-group-item-danger">
+                            Создать гайд
+                        </button>
+                    </div>
+                </div>
             </div>
         );
     }
