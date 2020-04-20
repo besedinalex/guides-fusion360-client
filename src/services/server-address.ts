0a641801815607e2efd3fe8f074c89d55a26d938
Alexey <besedinalexey@gmail.com>
2020-04-20 20:41:49 +0300

    Removed need to manually change server-url before prod

--- src/services/server-address.ts
+++ src/services/server-address.ts
@@ -1,2 +1,16 @@
-// TODO: Update before prod
-export const serverURL = `http://${window.location.hostname}:4004`;
+const url = window.location.hostname;
+
+let port;
+switch (window.location.port) {
+    case '3000': // Dev server
+        port = ':4004';
+        break;
+    case '': // Has domain name
+        port = '';
+        break;
+    default: // Has not domain name
+        port = `:${window.location.port}`;
+        break;
+}
+
+export const serverURL = `http://${url}${port}`;
